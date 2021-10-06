6591123ed3df89bb2a5766eb1e41edf4147cc763
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2021-10-05 22:20:32 -0500

    Enable support for GCC 11.2 for the RoboRIO for testing

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
@@ -2,9 +2,9 @@ package edu.wpi.first.toolchain.roborio;
 
 public class RoboRioToolchainExtension {
 
-    public String versionLow = "7.3";
-    public String versionHigh = "7.3";
-    public String toolchainVersion = "2022-7.3.0";
+    public String versionLow = "11.2";
+    public String versionHigh = "11.2";
+    public String toolchainVersion = "2022-11.2.0";
     public String toolchainTag = "v2022-1";
 
     public String year = "2022";
